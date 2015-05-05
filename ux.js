779efb18ceb2a60131e5d2359b50b87f9241d4ff
José Pedro <jose.pedro.dias@gmail.com>
2015-05-05 16:26:44 +0100

    ongoing ux

--- ux.js
+++ ux.js
@@ -10,14 +10,24 @@
 
     var tpls = {
         profile: function() {/*
-            <a class="itter itter-profile">
+            <a class="itter itter-profile" href="{{user}}/profile.json" target="_blank">
                 <div class="avatar" style="background-image: url('{{avatar}}')"></div>
                 <div class="avatar-rest">
                     <div class="name">{{name}}</div>
                     <div class="user">{{user}}</div>
                 </div>
-                <div class="desc">{{description}}</div>
-            </a>*/}
+                <div class="description">{{description}}</div>
+                <div class="languages">{{languages}}</div>
+            </a>*/},
+        post: function() {/*
+            <a class="itter itter-post" href="{{user}}" target="_blank">
+            </a>*/},
+        timeline: function() {/*
+            <a class="itter itter-timeline" href="{{url}}" target="_blank">
+            </a>*/},
+        form: function() {/*
+            <div class="itter itter-form">
+            </div>*/}
     };
 
     for (var k in tpls) {
@@ -35,33 +45,33 @@
         el.innerHTML = tpl;
     };
 
-    //applyTemplate(tpl1, {name:'Johnny', age:20});
 
 
+    // TODO TEMP
+    var profile = {
+        "name":        "User 1",
+        "avatar":      "https://secure.gravatar.com/avatar/5aecee0248f70bcbf79c82e8a87a4e78.jpg?s=128&d=https%3A%2F%2Fcodebits.eu%2Flogos%2Fdefaultavatar.jpg",
+        "languages":   "en,pt",
+        "description": "lorem ipsum"
+    };
+    var post = {};
+    var timeline = [];
 
     var renderProfile = function(user, sel) {
-        applyTpl(
-            'profile',
-            {
-                avatar:      'https://avatars0.githubusercontent.com/u/525733?s=140',
-                name:        'NAME',
-                user:        'USER',
-                description: 'DESC'
-            },
-            sel
-        );
+        profile.user = user;
+        applyTpl('profile', profile, sel);
     };
 
-    var renderPost = function(user, created_at, sel) {
-
+    var renderPost = function(post_url, sel) {
+        applyTpl('post', post, sel);
     };
 
     var renderTimeline = function(user, sel) {
-
+        applyTpl('timeline', timeline, sel);
     };
 
     var postForm = function(user, sel) {
-
+        applyTpl('form', {}, sel);
     };
 
 
